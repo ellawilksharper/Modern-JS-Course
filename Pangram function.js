1b3ee35c57ac0f24c5b5b50e5974bd648fd2ab58
Ella Wilks-Harper <ellawilksharper@hotmail.co.uk>
2020-10-13 13:56:42 +0100

    Updating name and adding function instructions

--- Pangram function.js
+++ Pangram function.js
@@ -1,5 +1,10 @@
+//A pangram is a sentence that contains every letter of the alphabet, like:
+// "The quick brown fox jumps over the lazy dog"
 
+//Write a function called isPangram, which checks to see if a given setence contains every latter of the alphabet. Make sure you ignore string casing!
 
+// isPangram('The five boxing wizards jump quickly') //true
+// isPangram('The five boxing wizards jump quick') //false
 
 function isPangram(sent) {
     //check that each letter of the alphabet is in the sentence
